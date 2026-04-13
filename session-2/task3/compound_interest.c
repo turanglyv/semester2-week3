1392d7e4e1a46b0d2335dd89e6c8cf861020a81d
turanglyv <zdqn0534@leeds.ac.uk>
2026-04-13 14:38:46 +0000

    aty

--- session-2/task3/compound_interest.c
+++ session-2/task3/compound_interest.c
@@ -1,25 +1,20 @@
-
-/*
- * Compute the final result of applying compound interest 
- * to a given deposit with a set interest rate and term.
- * We will assume interest is applied at the end of each year.
- */
-
 #include <stdio.h>
+#include <math.h>
 
-int main( void ) {
+int main(void) {
 
-    float deposit = 120.0;  // initial deposit
-    float rate = 0.05;      // interest rate
-    int term = 5;           // number of years
+    float deposit = 120.0;
+    float rate = 0.05;
+    int term = 5;
     float final_amount;
     float gain;
 
-    /*
-     * Implement the formula for compound interest.
-     * Print out the final amount and the gain compared to the initial deposit.
-     */
-    
+    final_amount = deposit * pow((1 + rate), term);
+
+    gain = final_amount - deposit;
+
+    printf("Final amount: %f\n", final_amount);
+    printf("Gain: %f\n", gain);
 
     return 0;
 }
